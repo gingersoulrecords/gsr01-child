48162e275e9fb43968746fc0b24e4b66c7461710
Dave Bloom <gingersoulrecords@gmail.com>
2018-09-02 14:36:24 -0400

    test grid styles

--- functions.php
+++ functions.php
@@ -5,7 +5,9 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
 
+//    wp_enqueue_style( 'hug-css', get_stylesheet_directory_uri().'/hugrid.css' );
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+//    wp_enqueue_script( 'hug-js', get_stylesheet_directory_uri().'/js/hugrid.js' );
    wp_enqueue_script( 'gsr-child-scripts', get_stylesheet_directory_uri().'/js/scripts.js' );
 }
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,162 +1,204 @@
+	jQuery.fn.reverse = [].reverse;
+
 	jQuery(document).ready(function(){
 		
 	   
-	//remove empty paragraphs
-	jQuery('p:empty, br').remove();
-	    
-	 jQuery('.soulscroller').appendTo('body');   
+		//remove empty paragraphs
+		jQuery('p:empty, br').remove();
 
-	
-	
-	}); 
+		 
+		 jQuery('.soulscroller').appendTo('body');   
+
+
+
+
+
+
+
+	});
+		 
 	
 	jQuery(window).load(function(){
 					
+
+	var scrollSelector = jQuery('.fl-row').filter(function(){
+		return jQuery(this).outerHeight() != 0;
+	});
+	
+	console.log(scrollSelector);
+
+	window.scrollSections = [];
+	window.reverseScrollSections = [];
+
+	scrollSelector.each(function(i){
+		
+		//cache this and assign vars
+		$this = jQuery(this);		
+        offsetTop = $this.offset().top;
+        node = $this[0].dataset.node;
+        height = $this.outerHeight();
+	        
+		//populate array	        
+	    window.scrollSections[i] = {
+	        offsetTop: Math.round(offsetTop),
+	        node: node,
+	        height: height
+	    };
+	})
+	
+	scrollSelector.reverse().each(function(i){
+		
+		//cache this and assign vars
+		$this = jQuery(this);		
+        offsetTop = $this.offset().top;
+        node = $this[0].dataset.node;
+        height = $this.outerHeight();
+	        
+		//populate array	        
+	    window.reverseScrollSections[i] = {
+	        offsetTop: Math.round(offsetTop),
+	        node: node,
+	        height: height
+	    };
+	})
+	
+	
+	console.log(window.reverseScrollSections);
+
+	var scrollTween = function scrollTweenFunction(theIndex){
+				TweenMax.to( window, .5, {
+					
+					scrollTo: {
+						y: window.scrollSections[theIndex].offsetTop,
+						offsetY: window.offsetByElementHeight,
+						autoKill:false,
+					},
+					ease:Power4.easeOut
+					
+				});
+	} 
+	
+	var reverseScrollTween = function reverseScrollTweenFunction(theIndex){
+				TweenMax.to( window, .5, {
+					
+					scrollTo: {
+						y: window.reverseScrollSections[theIndex].offsetTop,
+						offsetY: window.offsetByElementHeight,
+						autoKill:false,
+					},
+					ease:Power4.easeOut
+					
+				});
+	} 
+	
+	window.onCanvasHeight = jQuery('.oncanvas .ss-container').outerHeight();
+	
+	
+	jQuery('.soulscroller-down').click(function(e){
+		
+		e.preventDefault();
+		
+		
+		
+		//get the current scroll position		
+		var windowScrollTop = Math.round(jQuery(window).scrollTop());
+		//console.log(windowScrollTop);
+		
+		//loop through scrollSections and find the next section index
+		jQuery(window.scrollSections).each(function(i){
+			
 /*
-		jQuery(window).scroll(function() {
-				var scrollDistance = jQuery(window).scrollTop();
-	
-				jQuery('.fl-row').each(function(i) {
-						if (jQuery(this).position().top <= scrollDistance) {
-								jQuery('.fl-row.active').removeClass('active');
-								jQuery('.fl-row').eq(i).addClass('active');
-						}
-				});
-		}).scroll();	
+			console.log(this.offsetTop);
+			console.log(windowScrollTop);
 */
-		window.scrollAdd = 0;
-		
-		
-		
-		//when you click on the up button
-		jQuery('.soulscroller-up').click(function(){
-
-			console.log(window.scrollAdd);
 			
-			//first, get the scroll distance of the window.
-			window.scrollDistance = jQuery(window).scrollTop();
-			
-			if(window.isSoulScrolling == 'soulscrolling'){
-				window.scrollAdd = window.scrollAdd+1;
-			}
-			
-			//console.log('scrolldistance= '+window.scrollDistance);
-
-  				          
-            	jQuery('.fl-row').filter(function(){
-	            	return jQuery(this).outerHeight() > 0;
-            	}).each(function(i) {
-	            	    //console.log(i + ': '+jQuery(this).position().top);
-	            	    
-	            	    //
-						if (jQuery(this).position().top >= window.scrollDistance + window.offsetByElementHeight) {
-							//if it's not the first row,
-							if(i-1-window.scrollAdd > 0){
-								//dest is the row above it
-								window.activePrevRowDest = jQuery('.fl-row').filter(function(){
-	            	return jQuery(this).outerHeight() > 0;
-            	}).eq(i-1-window.scrollAdd).position().top;
-							}else{
-								window.activePrevRowDest = 0;
-							}
-						return false;
-						}
-				});
+			//if the scroll position matches a section, scroll the window to the following section (note the i+1)
+			if(this.offsetTop == windowScrollTop || this.offsetTop == (windowScrollTop+window.onCanvasHeight)){
+				console.log('match, scrolling to next section, index = '+(i+1));
+				console.log('offsetTop ='+this.offsetTop);
+				console.log('window scroll ='+windowScrollTop);
 				
-				//then scroll to the position of the previous row. As this is animating, we'll set the activerow to the destination row.
-				TweenMax.to( window, .33, {
-					
-					scrollTo: {
-						y: window.activePrevRowDest,
-						offsetY: window.offsetByElementHeight,
-						autoKill:false,
-					},
-					onStart:function(){
-						window.isSoulScrolling = 'soulscrolling';
-					},
-					onComplete:function(){
-						window.isSoulScrolling = 'not-soulscrolling';
-						window.scrollAdd = 0;
-					},
-					ease:Power4.easeOut
-				} );
+				scrollTween(i+1);
 	
+				return false;
 
-    			
-		});//end up
-		
-		//three rapid clicks is adding to scrolladd while the scroll position changes, causing an extra jump. temp solution - speed up the animations. long term - store a scroll distance and determine if it's being rapidly clicked before the animation ends - in which case, throttle scrolladd.
-		
-		
-		//when you click on the down button
-		jQuery('.soulscroller-down').click(function(){
-
-		
-
-			console.log(window.scrollAdd);
-			
-			//first, get the scroll distance of the window.
-			window.scrollDistance = jQuery(window).scrollTop();
-			
-			
-			   if(window.scrollDistance + jQuery(window).height() == jQuery(document).height()) {
-			 	  return false;
-			   }
-
-			
-			if(window.isSoulScrolling == 'soulscrolling'){
-				window.scrollAdd = window.scrollAdd+1;
-			}
-			
-			console.log('scrolldistance= '+window.scrollDistance);
-
-  				          
-            	jQuery('.fl-row').filter(function(){
-	            	return jQuery(this).outerHeight() > 0;
-            	}).each(function(i) {
-	            	    console.log(i + ': '+jQuery(this).position().top);
-	            	    
-	            	    //
-						if (jQuery(this).position().top > window.scrollDistance + window.offsetByElementHeight) {
-
-							//if it's not the last row,
-							if(i+window.scrollAdd < jQuery('.fl-row').length){
-								//dest is the row below it
-								window.activePrevRowDest = jQuery('.fl-row').filter(function(){
-	            	return jQuery(this).outerHeight() > 0;
-            	}).eq(i+window.scrollAdd).position().top;
-								return false;
-							}else{
-								window.activePrevRowDest = 0;
-								return false;
-
-							}
-						}
-				});
 				
-				//then scroll to the position of the previous row. As this is animating, we'll set the activerow to the destination row.
-				TweenMax.to( window, .33, {
-					
-					scrollTo: {
-						y: window.activePrevRowDest,
-						offsetY: window.offsetByElementHeight,
-						autoKill:false,
-					},
-					onStart:function(){
-						window.isSoulScrolling = 'soulscrolling';
-					},
-					onComplete:function(){
-						window.isSoulScrolling = 'not-soulscrolling';
-						window.scrollAdd = 0;
-					},
-					ease:Power4.easeOut
-				} );
-	
+			//if the scroll position is less than a section, scroll the window to that section.	
+			}else if(this.offsetTop > windowScrollTop ){
+				console.log('no match, scrolling to next section down, index = '+i);
+				console.log('offsetTop ='+this.offsetTop);
+				console.log('window scroll ='+windowScrollTop);
+				
+				scrollTween(i);
 
-    			
-		});//end down
+				return false;
+
+
+			}
+		});
+		
+	});
+	
+	jQuery('.soulscroller-up').click(function(e){
+		
+		e.preventDefault();
+		
+		//get the current scroll position
+		var windowScrollTop = Math.round(jQuery(window).scrollTop());
+		//console.log(windowScrollTop);
+		
+		if(windowScrollTop == 0){
+			return false;
+		}
+		
+		//loop through scrollSections and find the next section index
+		jQuery(window.reverseScrollSections).each(function(i){
+			
+/*
+			console.log(this.offsetTop);
+			console.log(windowScrollTop);
+*/
+			
+			//if the scroll position matches a section, scroll the window to the previous section (note the i-1)
+			if(this.offsetTop == windowScrollTop || this.offsetTop == (windowScrollTop+window.onCanvasHeight)){
+				console.log('match, scrolling to previous section, index = '+i);
+				console.log('offsetTop ='+this.offsetTop);
+				console.log('window scroll ='+windowScrollTop);
+				
+				reverseScrollTween(i+1);
+	
+				return false;
+
+				
+			//if the scroll position is more than a section, scroll the window to that section.	
+			}else if(this.offsetTop < windowScrollTop ){
+				console.log('no match, scrolling to next section up, index = '+i);
+				console.log('offsetTop ='+this.offsetTop);
+				console.log('window scroll ='+windowScrollTop);
+				
+				reverseScrollTween(i);
+
+				return false;
+
+
+			}
+		});
+		
+	});
+
+
+
+
+
+
+
+		
+		
+
 	
 	});
+	
+
 	
 	
 	
